97ccc2433e0337611830e868c06f11e0a7feb115
DhanashreeNaykodi <dhanashree.naykodi@coditas.com>
2025-07-22 17:21:24 +0530

    Update and rename Q9.java to LowercaseCount.java

--- LowercaseCount.java
+++ LowercaseCount.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Q9 {
+public class LowercaseCount {
 
     public static void main(String[] args) {
         try {
