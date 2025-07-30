9e2e88fc11b5f5d1f298ecef21480877097319c5
DhanashreeNaykodi <dhanashree.naykodi@coditas.com>
2025-07-30 11:09:17 +0530

    Update SortListUsingLambda.java

--- SortListUsingLambda.java
+++ SortListUsingLambda.java
@@ -7,10 +7,10 @@ public class SortListUsingLambda {
         emp.add(new Employee2("Charlie",45, 90000));
         emp.add(new Employee2("Alice",20, 85000));
 
-        emp.sort((a, b) -> a.getAge() - b.getAge());
+        emp.sort((a, b) -> a.age - b.age);
         System.out.println(emp.toString());
 
-        emp.sort((a,b) -> a.getSalary() - b.getSalary());
+        emp.sort((a,b) -> a.salary - b.salary);
         System.out.println(emp.toString());
     }
 }
