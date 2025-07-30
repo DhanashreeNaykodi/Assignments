701f108d9aaf31e8a557fd1dcc307d31e61dd6f4
DhanashreeNaykodi <dhanashree.naykodi@coditas.com>
2025-07-30 11:08:43 +0530

    Update Employee2.java

--- Employee2.java
+++ Employee2.java
@@ -8,13 +8,6 @@ public class Employee2 {
         this.age = age;
         this.salary = salary;
     }
-    public int getAge() {
-        return age;
-    }
-    public int getSalary() {
-        return salary;
-    }
-
     @Override
     public String toString() {
         return name + " " + age + " " + salary;
