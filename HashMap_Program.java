c2abac1bbaf0be495135d9ee98431a5f3138935a
DhanashreeNaykodi <dhanashree.naykodi@coditas.com>
2025-07-30 11:35:07 +0530

    Update HashMap_Program.java

--- HashMap_Program.java
+++ HashMap_Program.java
@@ -1,19 +1,36 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class HashMap_Program {
+    HashMap<Integer, String> map = new HashMap<>();
+
+    public void addData(int[] Ids, String[] names) {
+        for (int i = 0; i < Ids.length; i++) {
+            map.put(Ids[i], names[i]);
+        }
+    }
+
+    public void removeID(int id) {
+        if (map.remove(id) != null) {
+            System.out.println("ID " + id + " removed successfully.");
+        }
+    }
+
+    public void printMap() {
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println("ID: " + entry.getKey() + ", Name: " + entry.getValue());
+        }
+    }
 
     public static void main(String[] args) {
 
-        HashMap<Integer, String> map = new HashMap<>();
+        int[] ids = {101, 102, 103, 104, 105};
+        String[] names = {"Pooja", "Shruti", "Amay", "Amy", "Sakshi"};
 
-        map.put(101,"Pooja");
-        map.put(102,"Shruti");
-        map.put(103,"Amay");
-        map.put(104,"Amy");
-        map.put(105,"Sakshi");
+        HashMap_Program obj = new HashMap_Program();
+        obj.addData(ids,names);
 
-        map.remove(103);
-
-        System.out.println(map);
+        obj.removeID(103);
+        obj.printMap();
     }
 }
